c9e9f43e7298117a5a1378531313d3d1bf53e52b
kaigidwani <kpg8244@rit.edu>
2025-11-06 01:48:01 -0500

    Added all features for hw Simple Models. Getting an error with updating age by name that doc.name is undefined.

--- server/router.js
+++ server/router.js
@@ -16,6 +16,7 @@ const router = (app) => {
   app.get('/page1', controllers.page1);
   app.get('/page2', controllers.page2);
   app.get('/page3', controllers.page3);
+  app.get('/page4', controllers.page4);
   app.get('/getName', controllers.getName);
   app.get('/findByName', controllers.searchName);
 
@@ -30,8 +31,15 @@ const router = (app) => {
   // For example, a form submission to www.webpage.com/setName
   app.post('/setName', controllers.setName);
 
+  // When someone POSTS to /setDogName, call controllers.setDogName
+  // For example, a form submission to www.webpage.com/setDogName
+  app.post('/setDogName', controllers.setDogName);
+
   // When someone POSTS to /updateLast, call controllers.updateLast
   app.post('/updateLast', controllers.updateLast);
+
+  // When someone POSTS to /ageDogByName, call controllers.ageDogByName
+  app.post('/ageDogByName', controllers.ageDogByName);
 };
 
 // export the router function
